a8c3ca6c2eeaca0a12616984b526ceb603b1541a
syifa <syifachoirunurus@gmail.com>
2021-10-01 11:33:04 +0700

    update layout main3

---
+++ app/src/main/java/com/example/coba/Activity_new4.java
@@ -0,0 +1,14 @@
+package com.example.coba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_new4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new4);
+    }
+}
--- app/src/main/java/com/example/coba/MainActivity.java
+++ app/src/main/java/com/example/coba/MainActivity.java
@@ -2,13 +2,83 @@ package com.example.coba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+
+import com.example.coba.Activity2;
+import com.example.coba.Activity_new3;
+import com.example.coba.Activity_new4;
+import com.example.coba.Activity_new5;
+import com.example.coba.Activity_6;
+import com.example.coba.Activity_new7;
+import com.example.coba.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivinstagram, ivwhatsapp, ivtelegram, ivspotify, ivtwitter, ivyoutube;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivinstagram = findViewById(R.id.iv_instagram);
+
+        ivinstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, Activity2.class);
+                startActivity(intent);
+            }
+        });
+
+        ivwhatsapp = findViewById(R.id.iv_whatsapp);
+        ivwhatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, Activity_new3.class);
+                startActivity(intent);
+            }
+        });
+
+        ivtelegram = findViewById(R.id.iv_telegram);
+        ivtelegram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, Activity_new4.class);
+                startActivity(intent);
+            }
+        });
+
+        ivspotify = findViewById(R.id.iv_spotify);
+        ivspotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, Activity_new5.class);
+                startActivity(intent);
+            }
+        });
+
+        ivtwitter = findViewById(R.id.iv_twitter);
+        ivtwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, Activity_6.class);
+                startActivity(intent);
+            }
+        });
+
+        ivyoutube = findViewById(R.id.iv_youtube);
+        ivyoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, Activity_new7.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
